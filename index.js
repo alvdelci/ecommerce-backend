0e20138f3eaf77c960b731924af5dff7dd00f42a
alvdelci <delcimarioalvesdev@gmail.com>
2025-01-21 11:06:56 -0300

    feat: configurando cors e importacao das rotas

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3333
+const express = require('express');
+const cors = require('cors');
+const route = require('./src/routes');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+const app = express();
+const port = 3333;
+
+app.use(cors());
+app.use(express.json());
+app.use(route);
 
 app.listen(port, () => {
     console.log(`Server online on port ${port}`);
