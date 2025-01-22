72b785ba88048ff8a68074009e7a025283d63217
alvdelci <delcimarioalvesdev@gmail.com>
2025-01-21 21:56:16 -0300

    fix: corrigindo retorno do metodo save product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -3,7 +3,7 @@ const ProductSchema = require('../schemas/ProductSchema');
 const save = async (req, res) => {
     const product = req.body;
     const state = await ProductSchema.create(product);
-    return res.status(200).json({ message: "success", data: state.toJSON() });
+    return res.status(200).json({ message: "success", data: state });
 }
 
 const list = async (req, res) => {
