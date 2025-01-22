a3d1375aa5776bea6587bb079544824e6af09b64
alvdelci <delcimarioalvesdev@gmail.com>
2025-01-22 10:56:31 -0300

    feat: Adicionando middlewares de cadastro de cliente e verificacao de parametros

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,5 @@
-
-const verifyRequiredFields = (req, res, next) => {
+const CustomerSchema = require("../schemas/CustomerSchema");
+const verifyProductRequiredFields = (req, res, next) => {
     const { name, price, image, stock } = req.body;
 
     if (!name || !price || !image || !stock) {
@@ -8,6 +8,42 @@ const verifyRequiredFields = (req, res, next) => {
     next();
 }
 
+const verifyCustomerRequiredFields = (req, res, next) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: `Missing required fields` });
+    }
+    next();
+}
+
+const verifyEmailAlreadyExists = async (req, res, next) => {
+    const { email } = req.body;
+
+    try {
+        const customer = await CustomerSchema.findOne({ email });
+        if (customer) {
+            return res.status(400).json({ message: `Email already exists` });
+        }
+        next();
+    } catch (error) {
+        return res.status(400).json({ message: `Failed to get customer`, error: error.message });
+    }
+
+}
+
+const verifyParamId = (req, res, next) => {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).json({ message: `Missing required fields` });
+    }
+    next();
+}
+
 module.exports = {
-    verifyRequiredFields
+    verifyProductRequiredFields,
+    verifyCustomerRequiredFields,
+    verifyEmailAlreadyExists,
+    verifyParamId
 }
