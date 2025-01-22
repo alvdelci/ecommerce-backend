d49e55b00dce300e3eed63b82471a7a4df342bfa
alvdelci <delcimarioalvesdev@gmail.com>
2025-01-22 16:47:50 -0300

    test: adicionando testes unitarios para ProductController

---
+++ test/unit/controllers/ProductController.test.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const request = require("supertest");
+const app = require("../../../index");
+
+require("dotenv").config();
+
+/**Open mongodb connection */
+beforeEach(async () => {
+    await mongoose.connect(process.env.MONGO_URL);
+});
+
+/**Close mongodb connection */
+afterEach(async () => {
+    await mongoose.connection.close();
+});
+
+describe("POST /api/product", () => {
+    it("should create a product and return 201", async () => {
+        const res = await request(app).post("/api/product").send({
+            name: "Product 1",
+            price: 95.90,
+            image: "image url",
+            stock: 22,
+        });
+        expect(res.statusCode).toBe(201);
+        expect(res.body.message).toBe("success");
+    });
+
+    it("should failed to register product and return 400", async () => {
+        const res = await request(app).post("/api/product").send({
+            name: "Product 1",
+            price: 95.90,
+            image: "", // image is required
+            stock: 22,
+        });
+        expect(res.statusCode).toBe(400);
+        expect(res.body.message).toBe("Missing required fields");
+    });
+});
+
+describe("GET /api/product", () => {
+    it("should list products and return 200", async () => {
+        const res = await request(app).get("/api/product");
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toBe("success");
+    });
+});
+
+describe("GET /api/product/:id", () => {
+    it("should return the product and code 200", async () => {
+        const product = await request(app).post("/api/product").send({
+            name: "Product 1",
+            price: 95.90,
+            image: "image url",
+            stock: 22,
+        });
+        const res = await request(app).get(`/api/product/${product.body.data._id}`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toBe("success");
+    });
+
+    it("should return 404 to product not found", async () => {
+        const res = await request(app).get(`/api/product/612b8e5f9f5e4d0c6a4e7b7d`);
+        expect(res.statusCode).toBe(404);
+        expect(res.body.message).toBe("Product not found");
+    });
+});
