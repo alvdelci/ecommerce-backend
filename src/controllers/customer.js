14027553c5e38c2763e84ec9ab34c3fd242d5c26
alvdelci <delcimarioalvesdev@gmail.com>
2025-01-22 10:57:07 -0300

    feat: Adicionando controller de cadastro e get de client

---
+++ src/controllers/customer.js
@@ -0,0 +1,22 @@
+const CustomerSchema = require("../schemas/CustomerSchema");
+
+const save = async (req, res) => {
+    try {
+        const customer = await CustomerSchema.create(req.body);
+        res.status(201).json({ message: "success", data: customer });
+    } catch (error) {
+        res.status(400).json({ message: "Failed to register customer", error: error.message });
+    }
+}
+
+const get = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const customer = await CustomerSchema.findById(id);
+        res.status(200).json({ message: "success", data: customer });
+    } catch (error) {
+        res.status(400).json({ message: "Failed to get customer", error: error.message });
+    }
+}
+
+module.exports = { save, get }
