5f47f2cdec69532efe1310ccfa8f91bf7adb4ab8
alvdelci <delcimarioalvesdev@gmail.com>
2025-01-22 11:22:58 -0300

    feat: Declarando metodos como assincronos

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,20 +1,20 @@
 const CartSchema = require('../schemas/CartSchema');
 
-const get = (req, res) => {
+const get = async (req, res) => {
     try {
-        const cart = CartSchema.find({ _id: req.params.id });
+        const cart = await CartSchema.findById(req.params.id);
         if (!cart) {
             return res.status(404).json({ message: "Cart not found" });
         }
-        return res.status(200).json({ data: cart });
+        return res.status(200).json({ message: "success", data: cart });
     } catch (error) {
         return res.status(400).json({ message: "Failed to get cart", error: error.message });
     }
 }
 
-const save = (req, res) => {
+const save = async (req, res) => {
     try {
-        const state = CartSchema.create(req.body);
+        const state = await CartSchema.create(req.body);
         return res.status(200).json({ message: "success", data: state });
     } catch (error) {
         return res.status(400).json({ message: "Failed to register cart", error: error.message });
