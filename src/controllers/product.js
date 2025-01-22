ed6c864d1671c52ec85372ca91d92cecbd7c99a7
alvdelci <delcimarioalvesdev@gmail.com>
2025-01-21 21:21:49 -0300

    feat: adicionando metodo salvar produto

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,17 +1,24 @@
 const ProductSchema = require('../schemas/ProductSchema');
 
+const save = async (req, res) => {
+    const product = req.body;
+    const state = await ProductSchema.create(product);
+    return res.status(200).json({ message: "success", data: state.toJSON() });
+}
+
 const list = async (req, res) => {
     const products = await ProductSchema.find({});
-    return res.status(200).json({ data: products });
+    return res.status(200).json({ message: "success", data: products });
 }
 
 const detail = async (req, res) => {
     const { id } = req.params;
     const product = await ProductSchema.findById(id);
-    return res.status(200).json({ data: product.toJSON() });
+    return res.status(200).json({ message: "success", data: product });
 }
 
 module.exports = {
     list,
-    detail
+    detail,
+    save
 }
