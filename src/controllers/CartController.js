b81e85cbf2fd8e1d33537cb5648db420826f9901
alvdelci <delcimarioalvesdev@gmail.com>
2025-01-22 15:06:05 -0300

    refactor: Passando acesso ao banco para o CartService

--- src/controllers/CartController.js
+++ src/controllers/CartController.js
@@ -1,37 +1,19 @@
 const CartSchema = require('../schemas/CartSchema');
+const CartService = require('../services/CartService');
 
 const get = async (req, res) => {
-    try {
-        const cart = await CartSchema.findById(req.params.id);
-        if (!cart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-        return res.status(200).json({ message: "success", data: cart });
-    } catch (error) {
-        return res.status(400).json({ message: "Failed to get cart", error: error.message });
-    }
+    const cart = await CartService.get(req.params.id);
+    res.status(cart.code).json(cart);
 }
 
 const save = async (req, res) => {
-    try {
-        const state = await CartSchema.create(req.body);
-        return res.status(200).json({ message: "success", data: state });
-    } catch (error) {
-        return res.status(400).json({ message: "Failed to register cart", error: error.message });
-    }
+    const cart = await CartService.save(req.body);
+    res.status(cart.code).json(cart);
 }
 
 const remove = async (req, res) => {
-    try {
-        const cart = await CartSchema.findById(req.params.id);
-        if (!cart) {
-            return res.status(404).json({ message: "Cart not found" });
-        }
-        await CartSchema.deleteOne({ _id: req.params.id });
-        return res.status(200).json({ message: "Cart deleted", data: cart });
-    } catch (error) {
-        return res.status(400).json({ message: "Failed to delete cart", error: error.message });
-    }
+    const cart = await CartService.remove(req.params.id);
+    res.status(cart.code).json(cart);
 }
 
 module.exports = {
