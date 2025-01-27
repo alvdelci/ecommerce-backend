9bc51a65dadec996dda7a689841f53c8e6d37c62
alvdelci <delcimarioalvesdev@gmail.com>
2025-01-27 16:45:23 -0300

    feat: implementando cliente Redis para cache e metodos get e set

---
+++ src/cache/index.js
@@ -0,0 +1,35 @@
+import redis from "redis";
+
+const client = redis.createClient({
+    socket: {
+        host: "localhost",
+        port: 6379,
+        password: "docker",
+    },
+});
+
+client.on('connect', () => {
+    console.log('Connected to Redis');
+});
+
+client.on('error', (err) => {
+    console.log('Error: ' + err);
+});
+
+client.on('end', () => {
+    console.log('Disconnected from Redis');
+});
+
+const set = async (key, value) => {
+    await client.set(key, value);
+}
+
+const get = async (key) => {
+    const value = await client.get(key);
+    console.log(value);
+
+    await client.disconnect();
+    return value;
+}
+
+export { client, get, set };
